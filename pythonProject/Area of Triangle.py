a161b874c224d441b918a4ed3a8a4c2c972fbe85
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-07-16 01:39:49 +0600

    Create Area of Triangle.py

---
+++ pythonProject/Area of Triangle.py
@@ -0,0 +1,16 @@
+#calculate the area of Triangle.
+
+base=float(input("Enter the value of base: "))
+height=float(input("Enter the value of height: "))
+
+area=0.5*base*height
+
+print("The area of Triangle is: ",area)
+
+
+
+#calculate the area of Circle.
+radius=float(input("\n\nEnter the value of radius: "))
+area=3.1416*radius*radius
+
+print("The area of circle is: ",area)
